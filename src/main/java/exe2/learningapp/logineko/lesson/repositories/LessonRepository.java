4c78ca22e063448f50b7d97a586972f7576afad7
nn-hieu <nn.hieu0701@gmail.com>
2025-09-11 13:53:13 +0700

    Api find lessons by course id

--- src/main/java/exe2/learningapp/logineko/lesson/repositories/LessonRepository.java
+++ src/main/java/exe2/learningapp/logineko/lesson/repositories/LessonRepository.java
@@ -1,10 +1,14 @@
 package exe2.learningapp.logineko.lesson.repositories;
 
+import exe2.learningapp.logineko.lesson.entities.Course;
 import exe2.learningapp.logineko.lesson.entities.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long>, JpaSpecificationExecutor<Lesson> {
+    List<Lesson> findByCourse(Course course);
 }
