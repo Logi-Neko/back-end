e87160037b09f01cfbf1ce4ffe9441957c9f0902
nn-hieu <nn.hieu0701@gmail.com>
2025-09-24 13:35:10 +0700

    Use env

--- src/main/java/exe2/learningapp/logineko/authentication/service/PaymentServiceImpl.java
+++ src/main/java/exe2/learningapp/logineko/authentication/service/PaymentServiceImpl.java
@@ -2,6 +2,7 @@ package exe2.learningapp.logineko.authentication.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import vn.payos.PayOS;
 import vn.payos.type.CheckoutResponseData;
@@ -14,14 +15,20 @@ import vn.payos.type.PaymentData;
 public class PaymentServiceImpl implements PaymentService {
     private final PayOS payOS;
 
+    @Value("${payos.success-page}")
+    private String successPage;
+
+    @Value("${payos.failure-page}")
+    private String failurePage;
+
     @Override
     public String createPaymentLink(long orderCode, int amount, String description) throws Exception {
         PaymentData paymentData = PaymentData.builder()
                 .orderCode(orderCode)
                 .amount(amount)
                 .description(description)
-                .returnUrl("http://localhost:5500/success.html")
-                .cancelUrl("http://localhost:5500/cancel.html")
+                .returnUrl(successPage)
+                .cancelUrl(failurePage)
                 .build();
 
         CheckoutResponseData data = payOS.createPaymentLink(paymentData);
