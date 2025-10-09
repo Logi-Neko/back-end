3a09d422d74e144487498e3b6d2294d1aaa35ba5
nn-hieu <nn.hieu0701@gmail.com>
2025-10-09 21:47:24 +0700

    cors config

--- src/main/java/exe2/learningapp/logineko/authentication/config/CorsConfig.java
+++ src/main/java/exe2/learningapp/logineko/authentication/config/CorsConfig.java
@@ -18,7 +18,9 @@ public class CorsConfig {
         // --- FE local dev ---
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://127.0.0.1:5173",
+                "http://localhost:3000",
+                "http://127.0.0.1:3000"
         ));
 
         // --- Domain production ---
